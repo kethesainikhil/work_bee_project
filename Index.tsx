dd3a361a9be0f81c169d4f9d44e65c0e4ba6b1dd
kethesainikhil <70809367+kethesainikhil@users.noreply.github.com>
2025-09-21 12:52:58 +0530

    dfd

--- Index.tsx
+++ Index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/input" ;
 import {
   Search,
   Briefcase,
